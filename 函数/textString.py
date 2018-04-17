f4dbaf4fe9b39f9d3b6ec537333a68fbc11f4af0
YiliaHuiMeng <1187373371@qq.com>
2018-04-17 15:58:53 +0800

    函数

---
+++ 函数/textString.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def longest_side(a,b):
+	"""
+	Funciton to find the length of the longest side of a right triangle
+	:arg a:Side a of the triangle
+	:arg b:Side b of the triangle
+	
+	:return:Length of the longest side c as float
+	"""
+
+if __name__ == '__main__':
+	print(longest_side.__doc__)
+	print(longest_side(4,5))
